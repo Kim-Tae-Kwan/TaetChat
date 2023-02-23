0c17013f809aebfb00483b35796a6f6599c9d31a
Kim-Tae-Kwan <vodjaos@gmail.com>
2023-02-23 17:54:06 +0900

    project 생성

---
+++ src/test/java/com/ktk/taekChat/TaekChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ktk.taekChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaekChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
